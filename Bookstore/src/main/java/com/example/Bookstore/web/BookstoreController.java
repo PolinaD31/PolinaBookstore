1c6a8409884cd9edfff9657f54e292b38204f3e7
Polina Domracheva <sannydenek@gmail.com>
2023-09-13 08:09:56 +0300

    Adding the add function

--- Bookstore/src/main/java/com/example/Bookstore/web/BookstoreController.java
+++ Bookstore/src/main/java/com/example/Bookstore/web/BookstoreController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.Bookstore.model.Book;
 import com.example.Bookstore.model.BookstoreRepository;
 
 
@@ -28,5 +29,15 @@ public class BookstoreController {
 		return "redirect:../booklist";
 	}
 	
+	@RequestMapping(value = "/add")
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book());
+		return "addbook";
+	}
 	
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(Book book) {
+		repository.save(book);
+		return "redirect:booklist";
+	}
 }
